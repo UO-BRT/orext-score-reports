037ebd23de1728986a0636ed88303c90deb8d64a
Daniel Anderson <daniela@uoregon.edu>
2021-11-02 11:54:11 -0700

    better documentation - swap teacher for assessor
    again

--- api-setup.R
+++ api-setup.R
@@ -1,39 +1,46 @@
-#* Generate a student report for the ORExt
+#* Generate a student report for the ORExt.
 #* @param ssid The statewide student identifier. Should be numeric.
-#* @param year The year of the test. Should be in the form YYYY-YY
-#* @param name Student's name (first and last)
-#* @param content The test content area. Should be one of "ELA", "Math", or "Science"
-#* @param birth_date The student's birth date. 
+#* @param year The year of the test. Should be in the form YYYY-YY.
+#* @param name Student's name (first and last).
+#* @param content The test content area. Should be one of "ELA", "Math",
+#*   or "Science".
+#* @param birth_date The student's birth date.
+#* @param test_date The date the assessment was administered.
+#* @param assessor_name The name of the person administering the assessment.
+#* @param schid The statewide school identifier.
+#* @param sch_name The name of the school.
+#* @param distid The statewide district identifier.
+#* @param dist_name The name of the district.
+#* @param county_name The name of the county in which the school is located.
+#* @param score The student's scale score on the assessment.
 #* @serializer html
 #* @get /report
 
-function(ssid, year, name, grade, content, birth_date, test_date, 
-         teacher_name, schid, sch_name, distid, dist_name, countyid, 
-         county_name, score) {
-  
-  outfile <- paste0(ssid, '-', content, '.html')
-  
+function(ssid, year, name, grade, content, birth_date, test_date,
+         assessor_name, schid, sch_name, distid, dist_name,
+         county_name, score, return = "html") {
+
+  outfile <- paste0(ssid, "-", content, ".html")
+
   rmarkdown::render(
-    'score-report.Rmd',
+    "score-report.Rmd",
    output_file = outfile,
     params = list(
-      ssid = ssid, 
-      year = year, 
-      name = name, 
-      grade = grade, 
+      ssid = ssid,
+      year = year,
+      name = name,
+      grade = grade,
       content = content,
-      birthdate = birth_date, 
+      birthdate = birth_date,
       testdate = test_date,
-      teacher = teacher_name, 
-      school_id = schid, 
+      assessor = assessor_name,
+      school_id = schid,
       school_name = sch_name,
-      district_id = distid, 
+      district_id = distid,
       district_name = dist_name,
-      county_id = countyid,
-      county_name = county_name, 
+      county_name = county_name,
       score = score
     )
   )
-  
   readBin(outfile, "raw", n = file.info(outfile)$size)
 }
